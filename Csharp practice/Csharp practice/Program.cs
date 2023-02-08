95ac07b23e9d5679efab4c572023de8c084b6202
sienaria <gs4694@naver.com>
2023-02-08 21:19:21 +0900

    first commit

---
+++ Csharp practice/Csharp practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp_practice
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            var x = Convert.ToInt32("1010", 2);
+            var y = Convert.ToInt32("0101", 2);
+
+            var and = x & y;
+            Console.WriteLine($"{and}:{Convert.ToString(and, 2)}");
+
+            var or = x | y;
+
+
+
+        }
+    }
+}
